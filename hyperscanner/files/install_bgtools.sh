5d7f817aea15f7511373158a647ad936cc75e48d
robot <robot@scanner.bos.berkshiregrey.com>
2020-05-08 12:12:32 -0400

    adding few files

---
+++ hyperscanner/files/install_bgtools.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+echo "Installing bgtools"
+bash -c "$(curl -sSL https://artifactory.berkshiregrey.com/artifactory/generic-local/bg_tools/setup-developer/linux/setup-developer)"
+echo "Installing devbox"
+bgtool deps install devbox
+echo "creating workspace"
+bgtool ws create fuji-smalls -w ~/devel/ros_ws
+echo "build source codes"
+cd ~/devel/ros_ws
+catkin build hyper_scanner
+echo" source the environment"
+source ./devel/setup.bash
+echo "run the app"
+./devel/lib/hyper_scanner/bg_scanner_main ./src/bg_customer/customer_systems/systems/fuji_frankensystem/config/hyperscanner.yaml
